79505a90dd7423e6e53fa0a31b69ed9046e9b940
Abhishek Bharadwaj <abhishek.bharadwaj@healthifyme.com>
2018-06-11 11:28:00 +0530

    Something wrong with json

--- app/src/main/java/com/example/abhishek/gitexplorer/data/Repository.kt
+++ app/src/main/java/com/example/abhishek/gitexplorer/data/Repository.kt
@@ -1,24 +1,27 @@
 package com.example.abhishek.gitexplorer.data
 
+import android.util.Log
 import com.example.abhishek.gitexplorer.Util
 import io.reactivex.SingleObserver
 import io.reactivex.disposables.Disposable
 
 object Repository {
 
+    const val TAG = "Repository"
+
     fun getData() {
         Api.getPRs().compose(Util.applyIOSchedulers())
             .subscribe(object : SingleObserver<PRData> {
                 override fun onSuccess(t: PRData) {
-
+                    Log.d(TAG, t.title)
                 }
 
                 override fun onSubscribe(d: Disposable) {
-
+                    Log.d(TAG, "onSubscribed called")
                 }
 
                 override fun onError(e: Throwable) {
-
+                    Log.e(TAG, e.toString())
                 }
             })
     }
--- app/src/main/java/com/example/abhishek/gitexplorer/view/MainActivity.kt
+++ app/src/main/java/com/example/abhishek/gitexplorer/view/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.abhishek.gitexplorer.view
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.example.abhishek.gitexplorer.R
+import com.example.abhishek.gitexplorer.data.Repository
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Repository.getData()
     }
 }
